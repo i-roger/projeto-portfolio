77241fa9832dd6fa28ff998a6c192b39a30f177f
i-roger <mroger.dev@gmail.com>
2022-03-31 14:26:36 -0300

    Style Landing Page

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
 import logo from './logo.svg';
-import './App.css';
+import './App.scss';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <header></header>
+      <div className="container">
+        <div className="card">
+          <div className="content">
+            <h2>Simples Landing Page</h2>
+            <p>Minha primeira Landing Page utilizando ReactJs. Clique em Entrar e visualize! </p>
+              <a href="https://landing0page1.netlify.app/">Entrar</a>
+          </div>
+        </div>
+
+        <div className="card">
+          <div className="content">
+            <h2>Simples Landing Page</h2>
+            <p>Minha primeira Landing Page utilizando ReactJs. Clique em Entrar e visualize! </p>
+              <a href="https://landing0page1.netlify.app/">Entrar</a>
+          </div>
+        </div>
+        
+      </div>
     </div>
   );
 }
